c6c6c2fdc4f4719146d2242bd0856805e2f80a00
Roque <roquealbertosoto@gmail.com>
2018-08-07 01:21:02 -0400

    Begin work on transform.py

--- record/scan/transform.py
+++ record/scan/transform.py
@@ -1,1 +1,8 @@
-# Python
+# Python Script to perform a 4 point perspective transform on a photo
+# document (rectangle shape), converting it to birds-eye view.
+
+import numpy as np
+import cv2
+
+
+
